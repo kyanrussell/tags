2d8118ca58a68ba6713f82d5b0a1cdb84476301a
Kyan Russell <ksrussell@berkeley.edu>
2022-10-30 20:19:01 -0700

    button + image

--- tags/gui/gui.py
+++ tags/gui/gui.py
@@ -1,13 +1,3 @@
-# import subprocess
-# from PIL import Image
-# from time import sleep
-
-# with Image.open("../DSC09961.JPG") as img:
-#     img.show()
-#     subprocess.call(['xdotool', 'click', '1'])
-#     tag = input("Add tags?")
-#     print(tag)
-#     subprocess.call(['osascript', '-e', 'tell application "Preview" to quit'])
 from tags.logic.tags import add_tag, get_photos
 from tags.types.photo import Photo
 from tags.tag_types.species_code import SpeciesCode
@@ -21,19 +11,34 @@ HEIGHT = 900
 
 
 root = Tk()
+
+# ========< background image >=======================================================
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+
+root.geometry("%dx%d" % (width, height))
+
+image1 = Image.open("../DSC09961.JPG").resize((width, height))
+test = ImageTk.PhotoImage(image1)
+
+label1 = Label(image=test)
+label1.image = test
+
+# Position image
+label1.place(x=0, y=0)
+
+# ========< button >=================================================================
 canvas = Canvas(root, width = WIDTH, height = HEIGHT)
 canvas.pack()
-
-img = ImageTk.PhotoImage(Image.open("../DSC09961.JPG").resize((WIDTH, HEIGHT)))
-canvas.create_image(0, 0, anchor=NW, image=img)
 
 
 def add_tags_cmd():
    global entry
    string= entry.get()
    label.configure(text=string)
-   add_tag(photo=Photo("../DSC09961.JPG"), tag=SpeciesCode(string))
-   print(get_photos(SpeciesCode(string)))
+   species_code = SpeciesCode(SpeciesCode[string])
+   add_tag(photo=Photo("../DSC09961.JPG"), tag=species_code)
+   print(get_photos(species_code))
 
 #Initialize a Label to display the User Input
 label=Label(canvas, text="Add tags?", font=("Courier 22 bold"))
@@ -47,8 +52,7 @@ entry.pack()
 #Create a Button to validate Entry Widget
 Button(canvas, text= "Okay",width= 20, command=add_tags_cmd).pack(pady=20)
 
-
-
+# ========< main loop >=================================================================
 root.mainloop() 
 
 
